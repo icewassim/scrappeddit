d436c0b5ea7746ca66624fb8be0d3084cf19f703
icewassim <wassimbenameur@gmail.com>
2017-01-07 12:50:45 +0100

    jquery cdn

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 require.config({
 	paths: {
-		jQuery: 'bower_components/jquery/dist/jquery.min',
+		jQuery: '//code.jquery.com/jquery-2.2.4.min',
         angular: '//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular',
 		angularRoute: '//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-route.min'
 	},

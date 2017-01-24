89a9a0cb774a9c30bf7eb01b34142f96824490ee
L-Wiss <wassimbenameur@gmail.com>
2017-01-24 11:46:27 +0100

    html5mode

--- app/app.js
+++ app/app.js
@@ -24,10 +24,13 @@ require([
                 function config($locationProvider, $routeProvider) {
                     $routeProvider.
                         when("/r/:subreddit/comments/:threadId/:title/", {
-                            templateUrl: "app/core/core-view.html"
+                            templateUrl: "/scrappeddit/app/core/core-view.html"
                         })
                         .otherwise("/r/AskReddit/comments/5aih71/which_movie_would_be_boring_if_its_plot_is/");
 
+                    $locationProvider.html5Mode({
+                        enabled: true
+                    });
                 }
             ]);
 
